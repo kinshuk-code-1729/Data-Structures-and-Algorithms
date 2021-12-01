5d3fd1dcc0af08e74508f4b9aa236e142f9d46dc
Kinshuk Banerjee <kinshuk2002.king@gmail.com>
2021-12-02 00:52:45 +0530

    Update Exp-16.c

--- Exp-16.c
+++ Exp-16.c
@@ -1,14 +1,14 @@
 // Program to implement Bubble Sorting Technique into an Array
 #include<stdio.h>
 int main(){
-    int a[]={76,35,55,64,27},i,j,temp;
+    int a[]={76,35,55,64,27},i,j,temp,n=(sizeof(a)/sizeof(int));
     printf("Intially the Array Elements Are :");
-    for(i=0;i<5;i++){
+    for(i=0;i<n;i++){
         printf("\t%d",a[i]);
     }
     printf("\nSorting the Elements in ascending order.....");
-    for(i=0;i<4;i++){
-        for(j=0;j<(4-i);j++){
+    for(i=0;i<(n-1);i++){
+        for(j=0;j<(n-i-1);j++){
             if(a[j]>a[j+1]){
                 temp=a[j];
                 a[j]=a[j+1];
@@ -17,7 +17,7 @@ int main(){
         }
     }
     printf("\nSorted Array is :");
-    for(i=0;i<5;i++){
+    for(i=0;i<n;i++){
         printf("\t%d",a[i]);
     }
  return 0;
