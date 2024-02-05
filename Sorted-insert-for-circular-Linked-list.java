833c81f6dead43fe367f60a124e00eae89050248
Kinshuk Banerjee <kinshuk2002.king@gmail.com>
2024-02-06 00:05:31 +0530

    Update Sorted-insert-for-circular-Linked-list.java

--- Sorted-insert-for-circular-Linked-list.java
+++ Sorted-insert-for-circular-Linked-list.java
@@ -97,5 +97,37 @@ public class Main {
 
 // } Driver Code Ends
 
-
 //User function Template for Java
+class Solution {
+    public Node sortedInsert(Node head, int data) {
+        // code here
+        Node cur = head;
+        Node newNode = new Node(data);
+        
+        // CASE 1 : Empty Linked List
+        if(head == null){
+            newNode.next = newNode;
+            return newNode;
+        }
+        
+        // CASE 2 : New Node is smaller than head
+        else if(data <= head.data){
+            while(cur.next != head){
+                cur = cur.next;
+            }
+            cur.next = newNode;
+            newNode.next = head;
+            return newNode;
+        }
+        
+        // CASE 3 : New Node can be inserted in between the list
+        else{
+            while(cur.next != head && data > cur.next.data){
+                cur = cur.next;
+            }
+            newNode.next = cur.next;
+            cur.next = newNode;
+            return head;
+        }
+    }
+}
