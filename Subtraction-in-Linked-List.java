defa5d27a5b5c56d275d2a7aa8a99e3fe8b0f9b8
Kinshuk Banerjee <kinshuk2002.king@gmail.com>
2024-02-05 23:53:23 +0530

    Update Subtraction-in-Linked-List.java

--- Subtraction-in-Linked-List.java
+++ Subtraction-in-Linked-List.java
@@ -1,81 +1,3 @@
-//{ Driver Code Starts
-import java.util.Scanner;
-
-// Node Class
-class Node {
-    int data;
-    Node next;
-
-    Node(int x) {
-        data = x;
-        next = null;
-    }
-}
-
-public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
-
-        while (t-- > 0) {
-            int n = sc.nextInt();
-            Node start = null;
-            Node temp=null, r = null;
-
-            // Create linked list from the array arr[].
-            // Created linked list will be 1->2->11->56->12
-            if (n > 0) {
-                int arr = sc.nextInt();
-
-                temp = new Node(arr);
-                start = temp;
-                r = start;
-            }
-
-            for (int i = 0; i < n - 1; i++) {
-                int arr = sc.nextInt();
-                temp = new Node(arr);
-                r.next = temp;
-                r = r.next;
-            }
-
-            if (n > 0)
-            {
-                temp.next = start;
-                r = temp; 
-            }
-
-
-            int x = sc.nextInt();
-            Solution ob = new Solution();
-            start = ob.sortedInsert(start, x);
-            printList(start);
-            r = start;
-            while (r != start.next) {
-                temp = start;
-                start = start.next;
-                temp = null;
-            }
-            temp = null;
-        }
-    }
-
-    /* Function to print Nodes in a given linked list */
-    static void printList(Node start) {
-        Node temp;
-
-        if (start != null) {
-            temp = start;
-            do {
-                System.out.print(temp.data + " ");
-                temp = temp.next;
-            } while (temp != start);
-            System.out.println();
-        }
-    }
-}
-
-// } Driver Code Ends
 /*
 
 Definition for singly Link List Node
@@ -94,37 +16,104 @@ You can also use the following for printing the link list.
 Node.printList(Node node);
 */
 
+
 //User function Template for Java
 class Solution {
-    public Node sortedInsert(Node head, int data) {
-        // code here
-        Node cur = head;
-        Node newNode = new Node(data);
-        
-        // CASE 1 : Empty Linked List
-        if(head == null){
-            newNode.next = newNode;
-            return newNode;
+    public Node subLinkedList(Node head1, Node head2) {
+        // remove zeroes
+        while(head1 != null && head1.data == 0){
+            head1 = head1.next;
         }
         
-        // CASE 2 : New Node is smaller than head
-        else if(data <= head.data){
-            while(cur.next != head){
-                cur = cur.next;
+        while(head2 != null && head2.data == 0){
+            head2 = head2.next;
+        }
+        
+        int len1 = lsize(head1), len2 = lsize(head2);
+        
+        if(len1 == 0 && len2 == 0){
+            return new Node(0);
+        }
+        
+        if(len2 > len1){
+            Node tmp = head1;
+            head1 = head2;
+            head2 = tmp;
+        }
+        
+        if(len1 == len2){
+            Node cur1 = head1, cur2 = head2;
+            while(cur1.data == cur2.data){
+                cur1 = cur1.next;
+                cur2 = cur2.next;
+                
+                if(cur1 == null){
+                    return new Node(0);
+                }
             }
-            cur.next = newNode;
-            newNode.next = head;
-            return newNode;
+            if(cur2.data > cur1.data){
+                Node tmp = head1;
+                head1 = head2;
+                head2 = tmp;
+            }
         }
         
-        // CASE 3 : New Node can be inserted in between the list
-        else{
-            while(cur.next != head && data > cur.next.data){
-                cur = cur.next;
+        head1 = rev(head1);
+        head2 = rev(head2);
+        Node res = null;
+        
+        while(head1 != null){
+            int node1 = head1.data;
+            int node2 = 0;
+            if(head2 != null){
+                node2 = head2.data;
             }
-            newNode.next = cur.next;
-            cur.next = newNode;
-            return head;
+            
+            if(node1 < node2){
+                if(head1.next != null){
+                    head1.next.data -= 1;
+                }
+                node1 += 10;
+            }
+            
+            Node cur = new Node(node1 - node2);
+            cur.next = res;
+            res = cur;
+            
+            head1 = head1.next;
+            
+            if(head2 != null){
+                head2 = head2.next;
+            }
         }
+        
+        while(res != null && res.next != null){
+            if(res.data == 0){
+                res = res.next;
+            }
+            else{
+                break;
+            }
+        }
+        
+        return res;
+        
+    }
+    int lsize(Node head){
+        int c = 0;
+        for(Node cur=head; cur != null;cur = cur.next){
+            c += 1;
+        }
+        return c;
+    }
+    Node rev(Node head){
+        Node prev = null, cur = head,next = null;
+        while(cur != null){
+            next = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = next;
+        }
+        return prev;
     }
 }
