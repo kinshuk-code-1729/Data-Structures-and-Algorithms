0df058c37a922f183fde4674cf84aa5e2d433af5
Kinshuk Banerjee <kinshuk2002.king@gmail.com>
2024-04-11 23:57:35 +0530

    Update grayToBinary.java

--- grayToBinary.java
+++ grayToBinary.java
@@ -3,12 +3,10 @@ class Solution{
     
     // function to convert a given Gray equivalent n to Binary equivalent.
     public static int grayToBinary(int n) {
-        
-        // Your code here
         int res = 0;
         
         while(n > 0){
-            res ^= n;
+            res ^= n; // [(a ^ b) = c] => [(b ^ c) = a] and [(a ^ c) = b].  
             n >>= 1; // same as n = n / 2
         }
         
